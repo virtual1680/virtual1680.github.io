deca4843ab42a2a3ca0dc239d58fdda60efeda4b
virtual1680 <virtual1680@gmail.com>
2022-08-20 13:20:45 +0800

    feat:del

--- assets/components_form_index.md.03beb538.js
+++
@@ -1,7 +0,0 @@
-import{_ as e,c as t,o,f as n}from"./app.2e64142b.js";const f='{"title":"FORM \u6587\u6863","description":"","frontmatter":{},"headers":[{"level":2,"title":"FORM \u6587\u6863","slug":"form-\u6587\u6863"}],"relativePath":"components/form/index.md","lastUpdated":1659062130000}',a={},s=n(`<h2 id="form-\u6587\u6863" tabindex="-1">FORM \u6587\u6863 <a class="header-anchor" href="#form-\u6587\u6863" aria-hidden="true">#</a></h2><div class="warning custom-block"><p class="custom-block-title">WARNING</p><p>\u5728\u6848\u4F8B\u4E2D\u4EE5\u4E0B\u5199\u6CD5\u53EF\u4EE5\u5FFD\u7565,\u6309\u7167\u6B63\u5E38\u5199\u6CD5\u5373\u53EF,\u4EE5\u4E0B\u4EE3\u7801\u4EC5\u4EC5\u662F\u4E3A\u4E86\u5728\u6587\u6863\u4E2D\u5F15\u5165\u7B2C\u4E09\u65B9\u7EC4\u4EF6</p><div class="language-"><pre><code>const ElementPlus = ref()
-onMounted(() =&gt; {
-	import(&#39;element-plus&#39;).then(module =&gt; {
-		ElementPlus.value = module
-	})
-})
-</code></pre></div></div>`,2),r=[s];function c(l,d,_,i,p,m){return o(),t("div",null,r)}var h=e(a,[["render",c]]);export{f as __pageData,h as default};
